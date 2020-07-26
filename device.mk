6fd07ca13b5dd0c9fa44c662846c07103b7ce93f
Erfan Abdi <erfangplus@gmail.com>
2020-07-27 03:50:21 +0430

    ginkgo: Import stock FP input configs
    Change-Id: I1b07388f21370eef46a06e8e6bec1861ae4c4e5d

--- device.mk
+++ device.mk
@@ -25,6 +25,16 @@ PRODUCT_PACKAGES += \
     init.device.rc \
     init.fingerprint.rc
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    $(DEVICE_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
