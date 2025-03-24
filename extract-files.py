2ef4095508eaf6cb71693091d02dc85b22330a89
Skyblueborb <tomaszborbely0710@gmail.com>
2025-03-24 21:05:23 +0100

    ginkgo: Initial proprietary files list
    Fixups taken from laurel_sprout tree.
    
    Change-Id: Ia4ca977c0856d4923faf56356e302dcf692004b4
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -19,6 +19,7 @@ from extract_utils.main import (
 )
 namespace_imports = [
     'hardware/qcom-caf/sm8150',
+    'hardware/xiaomi',
     'vendor/qcom/opensource/display',
     'vendor/xiaomi/sm6125-common',
 ]
@@ -27,6 +28,25 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/miwatermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+    'vendor/lib64/libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1.so': blob_fixup()
+        .remove_needed('libhidlbase.so')
+        .replace_needed('libhidltransport.so', 'libhidlbase-v32.so'),
+    'vendor/lib/libalLDC.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
+        .clear_symbol_version('__aeabi_memcpy')
+        .clear_symbol_version('__aeabi_memset')
+        .clear_symbol_version('__gnu_Unwind_Find_exidx'),
+    'vendor/lib/libVDSuperPhotoAPI.so': blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
