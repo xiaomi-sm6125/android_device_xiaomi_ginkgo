f0707cc6f2d17e7db991108e8a0a1dab29e14d23
Skyblueborb <tomaszborbely0710@gmail.com>
2025-03-27 17:15:42 +0100

    ginkgo: Import overlays
    From https://github.com/LineageOS/android_device_xiaomi_ginkgo/tree/lineage-20/
    
    Change-Id: I5253ac2521d897ebf4fdfb0d1c625338a36d8ee3
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
